708b14fc2ffcd1f38cc9b5800c2c57211bfa8915
kathees <kathees@wso2.com>
2016-06-28 19:17:21 +0530

    Fixed to suppport ESB 5.0.0

--- src/test/java/org/wso2/carbon/connector/integration/test/dropbox/DropboxConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/dropbox/DropboxConnectorIntegrationTest.java
@@ -47,7 +47,7 @@ public class DropboxConnectorIntegrationTest extends ConnectorIntegrationTestBas
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("dropbox-connector-1.0.1-SNAPSHOT");
+        init("dropbox-connector-1.0.1");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
 
